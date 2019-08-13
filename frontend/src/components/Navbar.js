b26285cefe6f1b9330e83e37ce2686c6ff4bddfa
Nathaniel Sena <nathanielsena@Nathaniels-MacBook.local>
2019-08-12 23:16:42 -0700

    quickly added a post to get in the groove

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Navbar(){
+    return (
+        <div style={{width: '100%', backgroundColor: 'black'}}>
+            <button style={{float: 'right'}}>Login</button>
+        </div>
+    )
+}
+
+export default Navbar
---
+++ frontend/src/pages/Restricted.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Restricted(){
+    return (
+        <div>
+            <h1>You must be logged in to view this page</h1>
+        </div>
+    )
+}
+
+export default Restricted
