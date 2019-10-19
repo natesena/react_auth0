075bef1dfa3e984c98124235b1f41e893c121d13
Nathaniel Sena <nathanielsena@Nathaniels-MacBook-Pro.local>
2019-10-19 15:31:34 -0700

    changed some light things lets see if it works

--- index.js
+++ index.js
@@ -66,10 +66,10 @@ app.use("/api/posts", PostRouter);
 //test code
 if (process.env.NODE_ENV === "production") {
   // Serve any static files
-  app.use(express.static(path.join(__dirname, "frontend/build")));
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
   // Handle React routing, return all requests to React app
   app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "frontend/build", "index.html"));
+    res.sendFile(path.join(__dirname, "/frontend/build", "index.html"));
   });
 }
 //end test code
