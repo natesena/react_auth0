7e78a529b8f5c15b3aaa79c4e0cd7529767456cc
Nathaniel Sena <nathanielsena@Nathaniels-MacBook-Pro.local>
2019-10-27 23:17:49 -0700

    played around with styling of slidedown buttons

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -1,7 +1,12 @@
 import React, { useEffect, useState, memo } from "react";
 import { Link } from "react-router-dom";
+import { useAuth0 } from "../react-auth0-wrapper";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTimes } from "@fortawesome/free-solid-svg-icons";
+
 function Sidebar(props) {
   const [navClass, toggleVisibility] = useState("nav-sidebar");
+  const { isAuthenticated, user } = useAuth0();
 
   useEffect(() => {
     props.show
@@ -19,7 +24,7 @@ function Sidebar(props) {
             props.closeDropdown(false);
           }}
         >
-          Home
+          3D
         </Link>
         <Link
           to={"/posts"}
@@ -28,24 +33,53 @@ function Sidebar(props) {
             props.closeDropdown(false);
           }}
         >
-          Posts
+          Home
         </Link>
-        <Link
-          to={"/editor"}
-          className={"sidebar-link"}
-          onClick={() => {
-            props.closeDropdown(false);
-          }}
-        >
-          Editor
-        </Link>
+        {isAuthenticated &&
+          user["http://www.nateapp.comroles"].includes("admin") && (
+            <div>
+              <Link
+                to={"/editor"}
+                className={"sidebar-link"}
+                onClick={() => {
+                  props.closeDropdown(false);
+                }}
+              >
+                Editor
+              </Link>
+              <Link
+                to={"/login"}
+                className={"sidebar-link"}
+                onClick={() => {
+                  props.closeDropdown(false);
+                }}
+              >
+                Logout
+              </Link>
+            </div>
+          )}
+        {!isAuthenticated && (
+          <Link
+            to={"/login"}
+            className={"sidebar-link"}
+            onClick={() => {
+              props.closeDropdown(false);
+            }}
+          >
+            Login
+          </Link>
+        )}
+
         <div
-          className={"sidebar-close"}
+          className={"sidebar-link sidebar-close"}
           onClick={() => {
             props.closeDropdown(false);
           }}
         >
-          X
+          <FontAwesomeIcon
+            icon={faTimes}
+            style={{ filter: "drop-shadow(0 1px 2px rgba(75,75,75,1))" }}
+          />
         </div>
       </div>
     </div>
